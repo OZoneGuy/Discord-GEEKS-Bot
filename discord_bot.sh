e5b831dfb9381442ad41e48ca720f89bfee21053
OzoneGuy <alkershomar@protonmail.com>
2019-09-19 13:39:56 -0400

    Fixed text formatting

--- discord_bot.sh
+++ discord_bot.sh
@@ -3,6 +3,6 @@ do
 	python3 discord_bot.py
 	echo -e "\e[31mDiscod Bot Died.\e[0m"
 	date >> discord_bot.log
-	echo -e "\e[32,Restarting...\e[0m"
+	echo -e "\e[32mRestarting...\e[0m"
 	sleep 1
 done
