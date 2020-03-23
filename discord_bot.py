010011cbeefe438b41a056347eab1cdbf588831a
O-Zone-Guy <alkersho@protonmail.com>
2020-03-23 22:32:53 +0000

    fix - checks the channel before finding role

--- discord_bot.py
+++ discord_bot.py
@@ -169,6 +169,9 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
     guild:discord.Guild           = bot.get_guild(payload.guild_id)
     member : discord.Member       = guild.get_member(payload.user_id)
     channel : discord.TextChannel = bot.get_channel(payload.channel_id)
+    if payload.channel_id != role_channel or payload.channel_id != reg_channel:
+        return
+
     role : discord.Role = guild.get_role(roles_dic[emoji_name])
 
     if payload.message_id == role_message_id:
