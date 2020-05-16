bd5317fe4a0bd803bfa2fabbc570cecd4e50a15d
OzoneGuy <alkershomar@protonmail.com>
2020-05-15 23:12:24 -0400

    refactor - Added doc string and removed extra code

--- discord_bot.py
+++ discord_bot.py
@@ -1,63 +1,62 @@
-import discord
-from discord.ext import commands
+"""
+The main discord bot script.
+"""
+
 import json
 from datetime import datetime
 
-version = "0.19.10.19 A"
+from discord.ext import commands
+from welcome_cog import Welcome
+
+VERSION = "0.15.5.20 A"
 
 config = json.load(open("config.json"))
 
 # channel IDs
-reg_channel = 635285850015531028
-dev_channel = 385506783919079425
-role_channel = 635285966218592266
-general_channel = 378272844313919500
+DEV_CHANNEL = 385506783919079425
 
 
-# takes a text and logs into file with timestamp
 def write_log(text: str):
+    """
+    Write 'str' as a log into the log file.
+    """
     text = str(datetime.now()) + " - " + text + "\n"
-    f = open("GEEK.log", "a+")
-    f.write(text)
-    f.close()
+    f_log = open("GEEK.log", "a+")
+    f_log.write(text)
+    f_log.close()
+
 
 # bot client
-
-
 bot: commands.Bot = commands.Bot(command_prefix='$')
 
 
 # on ready function - logs ready time
 @bot.listen()
 async def on_ready():
+    """
+    Called when the bot is ready.
+    """
     print("TheGeek is ready.")
+    bot.add_cog(Welcome(bot))
     write_log("Ready.")
-
-
-@bot.listen()
-async def on_message(message: discord.Message):
-    '''
-    Called when a user sends a message in a chat the bot can access,
-    server channels, DMs, etc.
-    Not used for anything atm
-    '''
-    if message.author == bot.user:
-        return
-        pass
 
 
 async def in_dev(ctx: commands.Context) -> bool:
     '''
     checks if message came from dev
     '''
-    return ctx.channel.id == dev_channel
+    return ctx.channel.id == DEV_CHANNEL
 
 
 @bot.command()
 @commands.check(in_dev)
-async def hello(ctx):
+async def status(ctx):
+    """
+    A command used to check the status of the bot.
+    """
     print("test")
-    await ctx.channel.send(content="It's Alive!!!", delete_after=3)
-    pass
+    await ctx.channel.send(content="Up and Running. Version: " + VERSION,
+                           delete_after=3)
+
 
 bot.run(config['token'])
