ca943b3da48be9afa3b56fde513637343e3816a2
OzoneGuy <alkershomar@protonmail.com>
2020-05-21 22:06:56 -0400

    feat - Changed shell script logging message

--- discord_bot.sh
+++ discord_bot.sh
@@ -2,7 +2,7 @@ while :
 do
 	python3 discord_bot.py
 	echo -e "\e[31mDiscod Bot Died.\e[0m"
-	date >> discord_bot.log
+	echo "`date` - Bot died." >> GEEK.log
 	echo -e "\e[32mRestarting...\e[0m"
 	sleep 1
 done
