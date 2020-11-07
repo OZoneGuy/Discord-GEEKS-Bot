282d2975b1cdf3132f67dd9c194fad251ebaca9a
OZoneGuy <alkersho@protonmail.com>
2020-11-07 23:35:34 +0300

    feat - Added update method to sql_handler

--- src/sql_handler.py
+++ src/sql_handler.py
@@ -35,54 +35,5 @@ def is_registered(user_name, user_disc):
         return False
 
 
-def add_message(user_id):
-    _cursor.execute('SELECT message_cnt FROM user_lvl_n_msgs WHERE _user_id = {}'.format(user_id))
-    data = _cursor.fetchall()
-    if len(data) > 0:
-        message_cnt = data[0][0]
-        new_message_cnt = 1 + message_cnt
-        print(new_message_cnt)
-        _cursor.execute('UPDATE user_lvl_n_msgs SET message_cnt = {} WHERE _user_id = {}'.format(new_message_cnt, user_id))
-        _db.commit()
-    else:
-        _cursor.execute('INSERT INTO user_lvl_n_msgs (_user_id, message_cnt, level) VALUES({}, {}, {})'.format(user_id, 1, 1))
-        _db.commit()
-
-
-# checks if user is eady for level up
-def is_lvl_up(user_id):
-    _cursor.execute('SELECT message_cnt, level FROM user_lvl_n_msgs WHERE _user_id = {}'.format(user_id))
-    data = _cursor.fetchall()
-    if len(data) < 0:
-        print('error: user not in table user_lvl_n_msgs')
-        return False
-    if data[0][0] > messages_req_for_lvl(data[0][1]):
-        return True
-    return False
-
-
-def messages_req_for_lvl(cur_lvl):
-    return 2**(cur_lvl+1)
-    pass
-
-
-def get_messages(user_id):
-    _cursor.execute('SELECT message_cnt FROM user_lvl_n_msgs WHERE _user_id = {}'.format(user_id))
-    data = _cursor.fetchone()
-    return data[0]
-
-
-def get_level(user_id):
-    _cursor.execute('SELECT level FROM user_lvl_n_msgs WHERE _user_id = {}'.format(user_id))
-    data = _cursor.fetchone()
-    return data[0]
-
-
-# adds 1 to level
-def lvl_up(user_id):
-    _cursor.execute('SELECT level FROM user_lvl_n_msgs WHERE _user_id = {}'.format(user_id))
-    lvl = _cursor.fetchone()
-    new_lvl = 1 + lvl[0]
-    _cursor.execute('UPDATE user_lvl_n_msgs SET level = {} WHERE _user_id = {}'.format(new_lvl, user_id))
-    _db.commit()
-    print(new_lvl)
+def update():
+    sheets_interface.main()
