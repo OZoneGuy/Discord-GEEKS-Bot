4cf9468801bc13591fda364e53ff289aefa9ce1e
OZoneGuy <alkersho@protonmail.com>
2020-11-07 23:54:49 +0300

    feat - Improved script

--- discord_bot.sh
+++ discord_bot.sh
@@ -1,8 +1,8 @@
 while :
 do
-	python3 src/discord_bot.py
-	echo -e "\e[31mDiscod Bot Died.\e[0m"
-	echo "`date` - Bot died." >> data/GEEK.log
-	echo -e "\e[32mRestarting...\e[0m"
-	sleep 1
+    docker start geeks-bot
+    echo -e "\e[31mDiscod Bot Died.\e[0m"
+    echo "`date` - Bot died." >> data/GEEK.log
+    echo -e "\e[32mRestarting...\e[0m"
+    sleep 1
 done
