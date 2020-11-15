39ed7e17b1b5f27ba3875d92fc3f653e28daee72
OZoneGuy <alkersho@protonmail.com>
2020-11-15 03:45:55 +0300

    fix - Fixed get_member error

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -5,6 +5,7 @@ The main discord bot script.
 import json
 
 from discord.ext import commands
+from discord import Intents
 
 from bot_utils import get_config, write_log
 
@@ -16,8 +17,10 @@ config = json.load(open(get_config(), 'r'))
 DEV_CHANNEL = 385506783919079425
 
 
+intents = Intents.default()
+intents.members = True
 # bot client
-bot: commands.Bot = commands.Bot(command_prefix='$')
+bot: commands.Bot = commands.Bot(command_prefix='$', intents=intents)
 
 
 # on ready function - logs ready time
