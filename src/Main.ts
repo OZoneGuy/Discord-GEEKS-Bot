19c278cf2b07e994f2a0626c0989d2b08f8791d1
OZoneGuy <alkersho@protonmail.com>
2021-09-01 22:38:11 -0400

    feat - Written client boiler plate
    The client is what runs the bot

---
+++ src/Main.ts
@@ -0,0 +1,35 @@
+import "reflect-metadata";
+import { Client } from "discordx";
+import { Intents } from "discord.js";
+
+import { test_token as token } from "../config/secret_config.json";
+
+async function start() {
+    const client: Client = new Client({
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.DIRECT_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+        ],
+        requiredByDefault: true,
+        classes: [
+            `${__dirname}/events/*.{ts,js}`,
+            `${__dirname}/commands/*.{ts,js}`,
+            `${__dirname}/guards/*.{ts,js}`,
+        ]
+    });
+
+    client.once("ready", async () => {
+        await client.initApplicationCommands();
+        console.log("Bot is logged in!");
+    });
+
+    client.on("interactionCreate", (interaction) => {
+        client.executeInteraction(interaction);
+    });
+
+    await client.login(token);
+}
+
+start();
