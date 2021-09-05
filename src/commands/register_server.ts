85303b4b4e8c1340c16137bda47c1f471bd6fac1
OZoneGuy <alkersho@protonmail.com>
2021-09-04 21:02:23 -0400

    feat - Implemented `register_server` command

---
+++ src/commands/register_server.ts
@@ -0,0 +1,25 @@
+import { CommandInteraction, Guild, Permissions } from "discord.js";
+import { Discord, Guard, Slash } from "discordx";
+import { isAdmin } from "../gaurds/admin";
+import { add_guild } from '../utils/database/db'
+
+@Discord()
+@Guard(isAdmin)
+abstract class AppDiscord {
+    @Slash("register_server")
+    private register_server(interaction: CommandInteraction): void {
+        if (!interaction.inGuild) {
+            interaction.reply("Please use this command from the server you want to register")
+            return
+        }
+
+        const guild: Guild = interaction.guild!
+        add_guild(guild.id, guild.name).then((res) => {
+            if (res) {
+                interaction.reply(`Successfully added ${guild.name} to the database!`)
+            } else {
+                interaction.reply(`Failed to add ${guild.name} to the database!`)
+            }
+        })
+    }
+}
