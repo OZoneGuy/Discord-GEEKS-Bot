d4ac609a04fdbf4a64bfb67df9922269c303d24a
OZoneGuy <alkersho@protonmail.com>
2021-09-11 20:59:15 -0400

    feat - Implemented `set_guest_role`

---
+++ src/commands/set_guest_role.ts
@@ -0,0 +1,30 @@
+import { CommandInteraction, Role } from "discord.js";
+import { Discord, Guard, Slash, SlashOption } from "discordx";
+import { isAdmin } from "../gaurds/admin";
+import { set_guest_role } from "../utils/database/db";
+
+@Discord()
+@Guard(isAdmin)
+abstract class AddDiscord {
+    @Slash("set_guest_role")
+    private set_guest_role(
+        @SlashOption("Role", { required: true }) role: Role,
+        interaction: CommandInteraction) {
+
+        const guild_id = interaction.guild!.id
+        set_guest_role(guild_id, role.id).then((res) => {
+            switch (typeof res) {
+                case "boolean":
+                    if (res) {
+                        interaction.reply(`Successfully set the member's role to ${Role.name}`)
+                    } else {
+                        interaction.reply(`Failed to set the member's role`)
+                    }
+                    break;
+                default:
+                    interaction.reply(`Could not find the guild, make sure that you used \`/register_server\``)
+                    break;
+            }
+        })
+    }
+}
