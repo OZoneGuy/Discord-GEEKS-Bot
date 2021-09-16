47a2bb56326642a550d74432c74e433e5256c3d0
OZoneGuy <alkersho@protonmail.com>
2021-09-15 20:16:57 -0400

    fix - Fixed intents and added required partials
    Removed unnecessary intents and added partials required for some intents to work

--- src/Main.ts
+++ src/Main.ts
@@ -9,10 +9,11 @@ async function start() {
     const client: Client = new Client({
         intents: [
             Intents.FLAGS.GUILDS,
-            Intents.FLAGS.DIRECT_MESSAGES,
             Intents.FLAGS.GUILD_MESSAGES,
-            Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.DIRECT_MESSAGES,
         ],
+        partials: ["REACTION", "MESSAGE", "CHANNEL"],
         requiredByDefault: true,
         classes: [
             `${__dirname}/events/*.{ts,js}`,
