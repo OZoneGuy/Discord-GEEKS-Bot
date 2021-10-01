d953c978b2a4fc9e56158a9b528d84caf91a1d21
OZoneGuy <alkersho@protonmail.com>
2021-09-30 22:08:52 -0400

    chore - Removed old shell file

--- discord_bot.sh
+++
@@ -1,8 +0,0 @@
-while :
-do
-    python src/discord_bot.py
-    echo -e "\e[31mDiscod Bot Died.\e[0m"
-    echo "`date` - Bot died." >> data/GEEK.log
-    echo -e "\e[32mRestarting...\e[0m"
-    sleep 1
-done
